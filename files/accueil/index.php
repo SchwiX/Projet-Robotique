1f8139f94fd0db1164ebb510a430bf6861775fa3
Loic <loic.frossard@ceff.ch>
2019-01-10 17:48:02 +0100

    Finalisation du design Accueil

--- files/accueil/index.php
+++ files/accueil/index.php
@@ -39,24 +39,27 @@
             </div>
         </div>
     </header>
-    <div class="container ">
-        <h2>Bonjour et bienvenue sur notre site</h2>
+    <div class="container body">
+        <h2 class="titre-texte">Bonjour et bienvenue sur notre site</h2>
         <div class="text">
+            <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget diam cursus, ultricies turpis varius, lacinia erat. Curabitur mauris quam, tempus ut lacus eget, aliquam iaculis orci. Pellentesque ac sapien magna. Maecenas ut magna semper, faucibus nulla at, malesuada erat. Fusce eget enim quis purus viverra accumsan non aliquet elit. Morbi dapibus, est quis iaculis tincidunt, nunc velit venenatis tortor, sed tincidunt magna est quis erat. In tempus rhoncus lorem quis tincidunt. In quis est lobortis, sodales arcu sed, molestie elit. Mauris eget pharetra orci, et condimentum quam. Mauris consequat commodo lectus. Praesent tincidunt mattis dolor, et efficitur ipsum cursus at. Sed euismod luctus tristique. Donec sed vulputate nisi. Nulla in bibendum diam. Vestibulum ut egestas urna, at interdum lorem.
-
+            </p>
+            <p>
             Vestibulum rhoncus gravida mollis. Aliquam id augue turpis. Fusce sollicitudin faucibus lacus nec suscipit. Fusce sed elit fringilla lectus volutpat condimentum vel quis nunc. Nullam sit amet urna dictum, laoreet neque at, posuere leo. Vestibulum magna nisi, congue eget libero a, tincidunt luctus arcu. Quisque dapibus blandit lorem vitae ornare. Fusce nec ipsum ut eros porttitor varius feugiat scelerisque ipsum. Nulla ac accumsan ipsum, vel commodo est. Fusce tempus egestas neque, eu accumsan leo hendrerit a. Cras aliquet sed metus nec interdum. Maecenas in augue congue tellus ultrices vehicula. Proin molestie mollis mauris, vitae consequat diam faucibus nec.
-
+            </p>
+            <p>
             Nulla facilisi. Suspendisse turpis erat, malesuada vel gravida eget, rutrum at nibh. Cras quis viverra nulla. Praesent eget tortor placerat, vestibulum libero egestas, condimentum lectus. Quisque congue est arcu, porttitor ullamcorper ante aliquam sed. Aliquam eros lectus, faucibus nec nisi scelerisque, ullamcorper bibendum nunc. Donec placerat vestibulum mi, sit amet ultricies lorem. Vivamus ac neque sed risus mattis tincidunt a non risus. Mauris venenatis mauris ligula, eu rhoncus neque tincidunt et. Suspendisse sed dignissim magna, vitae maximus tortor. Nam fringilla elementum quam in viverra. Aliquam commodo pretium justo eu faucibus. In et ante dolor. Praesent tincidunt ut nulla a sagittis.
-
+            </p>
+            <p>
             Aliquam erat volutpat. Cras velit diam, dapibus at odio eu, suscipit condimentum lectus. In id odio elementum, mollis tortor id, varius enim. Vivamus non pulvinar lorem, quis dictum dolor. Mauris ornare turpis nec erat blandit, ac mattis massa suscipit. Quisque a lectus quis dui pulvinar luctus. Praesent dictum pretium magna, in efficitur sapien. Morbi non lorem purus. Quisque accumsan commodo quam, nec lacinia felis aliquam sit amet. Proin vel mauris sit amet sapien pellentesque sollicitudin venenatis eu eros. Phasellus a arcu dui. Sed ipsum odio, commodo sit amet bibendum quis, volutpat non nisi. Fusce massa purus, suscipit et laoreet in, volutpat sed dui. Praesent urna massa, feugiat nec molestie in, lobortis id tortor. Pellentesque malesuada pretium convallis.
-
+            </p>
+            <p>
             Ut maximus magna lectus, vel ultrices nulla feugiat sed. Donec ac elementum elit. Curabitur ipsum eros, aliquam a ante et, tincidunt placerat nunc. Suspendisse hendrerit, arcu sit amet commodo elementum, erat sem volutpat neque, non lobortis metus libero semper ante. Nunc finibus semper lorem et pretium. Praesent non ligula sem. Suspendisse dapibus, metus eget vulputate accumsan, neque massa cursus diam, vitae mollis dolor lectus non mi. Mauris in tellus non turpis bibendum imperdiet.
-
+            </p>
+            <p>
             Duis convallis in tortor non blandit. Vivamus et orci nibh. Donec nec metus odio. Curabitur posuere nulla ac ligula eleifend, eget molestie ex euismod. Proin ut ex consequat, pharetra justo lacinia, cursus nisi. Phasellus vulputate arcu in ex tempus mattis. Sed in consectetur est. Integer blandit est ac dolor dapibus, non auctor velit blandit. Duis sem velit, volutpat sed feugiat ac, convallis id tellus. Donec eget sagittis lorem. Donec magna sapien, tincidunt quis dolor nec, iaculis cursus quam. Praesent massa metus, consequat non ex in, pulvinar vestibulum neque. Pellentesque eget sollicitudin dolor, at suscipit nisi.
-
-            Duis laoreet feugiat mauris, fringilla pulvinar velit congue in. Pellentesque consectetur hendrerit metus, vel interdum augue mattis ac. Fusce egestas porta viverra. Morbi quis mi id nunc placerat facilisis. Maecenas efficitur id sem quis tristique. In ac lorem tincidunt, mollis nunc non, dignissim neque. Etiam a tristique ligula. Praesent consequat lobortis ornare. Mauris imperdiet convallis libero consectetur tincidunt. Mauris at consequat mi. Duis sit amet pulvinar eros. Fusce quis metus eu justo venenatis efficitur at eget nisi. Praesent imperdiet posuere lorem ut ornare. Phasellus posuere diam ex, et porttitor magna commodo sit amet. Suspendisse eget erat nibh.
-
-            Etiam mattis fermentum sapien eget porta. Morbi vitae erat at urna volutpat efficitur vel sit amet augue. Vivamus vel dapibus mauris. Nullam ac neque aliquet, mattis sem eu, euismod eros. Nulla facilisi. Nullam id facilisis lorem. Suspendisse commodo commodo elit, ut interdum nulla vestibulum elementum. Vestibulum auctor quam diam, a varius elit scelerisque a. Duis gravida ante eget ligula consequat feugiat.
+            </p>
         </div>
     </div>
 </body>
